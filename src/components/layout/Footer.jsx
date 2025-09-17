72dcb63b4313322c46d0c18eaecab0f3efc1831c
jaydeep4884 <sagathiyajaydeep66@gmail.com>
2025-09-17 09:05:33 +0530

    footer complete

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,11 +1,11 @@
 import { BsLinkedin, BsTelegram, BsTwitter } from "react-icons/bs";
 import { Link } from "react-router";
-import PageContainer from "./PageContainer";
 import DarkBrand from "../img/logo/brand.png";
 import LightBrand from "../img/logo/lightBrand.png";
+import Container from "@mui/material/Container";
 
 function Footer() {
-  const navLinks = ["Home", "Insights", "Career", "About"];
+  const navLinks = ["Journal", "Timelines", "Analytics", "Feedback"];
   const socialIcons = [
     { icon: <BsTwitter className="w-5 h-5" />, href: "/" },
     { icon: <BsTelegram className="w-5 h-5" />, href: "/" },
@@ -13,14 +13,14 @@ function Footer() {
   ];
 
   return (
-    <PageContainer>
-      <footer className="relative overflow-hidden">
+    <Container maxWidth="lg">
+      <footer className="relative">
         {/* Content */}
-        <div className="relative z-1 px-6">
+        <div className="relative z-1">
           <div className="py-6 md:py-12">
             <div className="flex flex-col md:flex-row justify-between gap-14">
               {/* Left Section */}
-              <div>
+              <div className="flex flex-col items-center sm:items-start ">
                 <img
                   src={LightBrand}
                   alt="Logo"
@@ -32,41 +32,32 @@ function Footer() {
                   className="hidden dark:block h-12 mb-4"
                 />
                 <p className="text-blue-400 font-medium">
-                  Not just words, but emotions captured 🖌️
+                  Not just words, but emotions captured
                 </p>
 
-                <div className="flex flex-wrap gap-6 pt-10">
-                  {navLinks.map((link, i) =>
-                    link === "About" ? (
-                      <Link key={i} href="/about">
-                        <span className="text-sm sm:text-base text-gray-300 hover:text-white transition-colors cursor-pointer">
-                          {link}
-                        </span>
-                      </Link>
-                    ) : (
-                      <span
-                        key={i}
-                        className="text-sm sm:text-base text-gray-300 hover:text-white transition-colors cursor-pointer"
-                      >
-                        {link}
-                      </span>
-                    )
-                  )}
+                <div className="flex flex-col sm:flex-row items-center  gap-6 pt-5">
+                  {navLinks.map((link, i) => (
+                    <p
+                      key={i}
+                      className="text-sm sm:text-base dark:text-gray-300 cursor-pointer"
+                    >
+                      {link}
+                    </p>
+                  ))}
                 </div>
               </div>
-
               {/* Right Section */}
-              <div>
+              <div className="flex flex-col items-center sm:items-start">
                 <p className="text-lg font-semibold mb-3">Contact Us</p>
                 <Link
                   href="mailto:contact@auraui.com"
-                  className="text-gray-300 hover:text-white text-sm sm:text-base transition-colors"
+                  className="dark:text-gray-300 hover:text-gray-400 text-sm sm:text-base transition-colors"
                 >
-                  contact@auraui.com
+                  jaydeepcreatives2024@gmail.com
                 </Link>
 
-                <div className="flex flex-row items-center gap-4 pt-8">
-                  <p className="text-sm sm:text-base text-gray-400">
+                <div className="flex flex-row items-center gap-4 pt-5">
+                  <p className="text-sm sm:text-base dark:text-gray-400">
                     Follow us:
                   </p>
                   <div className="flex gap-3">
@@ -75,7 +66,7 @@ function Footer() {
                         key={idx}
                         href={item.href}
                         target="_blank"
-                        className="w-10 h-10 flex items-center justify-center rounded-full border border-gray-700 bg-white/5 backdrop-blur-sm hover:bg-blue-500 hover:border-blue-500 hover:text-white text-gray-300 transition-all duration-300"
+                        className="w-10 h-10 flex items-center justify-center rounded-full border border-gray-700 bg-white/5 backdrop-blur-sm hover:bg-blue-500 hover:border-blue-500 hover:text-white dark:text-gray-300 transition-all duration-300"
                       >
                         {item.icon}
                       </Link>
@@ -92,7 +83,7 @@ function Footer() {
           Design & Developed By Sagathiya Jaydeep 💖
         </div>
       </footer>
-    </PageContainer>
+    </Container>
   );
 }
 
